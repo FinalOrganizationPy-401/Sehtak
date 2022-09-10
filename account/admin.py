1a9f38df687b48349f22f12ab6474f048fa0cbe1
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-09-10 14:18:35 +0300

    update: add Labs, X_ray, Pharmacist profile

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import PatientProfile,DoctorProfile, User
+from .models import PatientProfile,DoctorProfile, User, PharmacistProfile,X_rays_labProfile,LabsProfile
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(PatientProfile)
 admin.site.register(DoctorProfile)
+admin.site.register(PharmacistProfile)
+admin.site.register(X_rays_labProfile)
+admin.site.register(LabsProfile)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,4 @@
+from weakref import proxy
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
@@ -9,9 +10,9 @@ class User(AbstractUser):
         ADMIN = "ADMIN", "Admin"
         PATIENT = "PATIENT", "Patient"
         DOCTOR = "DOCTOR", "Doctor"
-        # PHARMACIST = "PHARMACIST", "Pharmacist"
-        # LABS = "LABS", "Labs"
-        # X_RAYS_LAB = "X_RAYS_LAB", "X_rays_lab"
+        PHARMACIST = "PHARMACIST", "Pharmacist"
+        LABS = "LABS", "Labs"
+        X_RAYS_LAB = "X_RAYS_LAB", "X_rays_lab"
 
     base_role = Role.ADMIN
 
@@ -22,6 +23,7 @@ class User(AbstractUser):
             self.role = self.base_role
             return super().save(*args, **kwargs)
 
+# Patient Profile
 
 class PatientManager(BaseUserManager):
     def get_queryset(self, *args, **kwargs):
@@ -33,13 +35,10 @@ class Patient(User):
 
     base_role = User.Role.PATIENT
 
-    student = PatientManager()
+    patient = PatientManager()
 
     class Meta:
         proxy = True
-
-    def welcome(self):
-        return "Only for Patient"
 
 
 @receiver(post_save, sender=Patient)
@@ -54,7 +53,7 @@ class PatientProfile(models.Model):
 
 
 
-# DOCTOR = "DOCTOR", "Doctor"
+# DOCTOR Profile
 
 class DoctorManager(BaseUserManager):
     def get_queryset(self, *args, **kwargs):
@@ -82,3 +81,88 @@ class DoctorProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created and instance.role == "DOCTOR":
        DoctorProfile.objects.create(user=instance)
+
+
+# Pharmacist Profile
+class PharmacistManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.PHARMACIST)
+
+class Pharmacist(User):
+
+    base_role = User.Role.PHARMACIST
+    Pharmacist = PharmacistManager()
+
+    class Meta:
+        proxy = True
+
+@receiver(post_save,sender=Pharmacist)
+def create_user_profile(sender, instance, created,*args, **kwargs):
+    if created and instance.role == "PHARMACIST":
+        PharmacistProfile.objects.create(user=instance)
+
+class PharmacistProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    pharmacist_id = models.IntegerField(null=True,blank=True)
+
+
+# Labs Profile
+
+class LabsManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.LABS)
+
+
+class Labs(User):
+
+    base_role = User.Role.LABS
+
+    Labs = LabsManager()
+
+    class Meta:
+        proxy = True
+
+
+
+class LabsProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    labs_id = models.IntegerField(null=True, blank=True)
+
+
+@receiver(post_save, sender=Labs)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "LABS":
+       LabsProfile.objects.create(user=instance)
+
+
+
+# X_rays_lab Profile
+
+class X_rays_labManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.X_RAYS_LAB)
+
+
+class X_rays_lab(User):
+
+    base_role = User.Role.X_RAYS_LAB
+
+    x_rays_lab = X_rays_labManager()
+
+    class Meta:
+        proxy = True
+
+
+
+class X_rays_labProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    x_rays_lab_id = models.IntegerField(null=True, blank=True)
+
+
+@receiver(post_save, sender=X_rays_lab)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "X_RAYS_LAB":
+       X_rays_labProfile.objects.create(user=instance)
