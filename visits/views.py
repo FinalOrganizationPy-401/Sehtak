5b012bc2ec42c867d015d06d3d2ab45f292dc261
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-09-18 14:23:43 +0300

    update : all visits related to the users now can be accessed

--- visits/views.py
+++ visits/views.py
@@ -1,6 +1,6 @@
 # import imp
 # from django.shortcuts import render
-from account.models import PatientProfile
+from account.models import DoctorProfile, LabsProfile, PatientProfile, PharmacistProfile, X_rays_labProfile
 from rest_framework.generics import RetrieveUpdateDestroyAPIView,CreateAPIView,ListAPIView
 
 from .serializers import VisitsSerializer
@@ -26,10 +26,37 @@ class PatientVisitsView(ListAPIView):
     Get all visits related for one user 
     '''
     def get_queryset(self):
-        patient = PatientProfile.objects.get(user=self.request.user.id)
-        data = Visits.objects.filter(patient=patient)
-        print(data)
-        return data
+        # patient =''
+        if self.request.user.role == 'DOCTOR':
+            patient = DoctorProfile.objects.get(user=self.request.user.id)
+            patient = patient.id
+            print(patient,'>>>>>>>>>>>>>>>>>')
+            data = Visits.objects.filter(doctor=patient)
+            print(data)
+            return data
+
+        elif self.request.user.role == 'PATIENT':
+            patient = PatientProfile.objects.get(user=self.request.user.id)
+            data = Visits.objects.filter(patient=patient)
+            return data
+
+        elif self.request.user.role == 'LABS':
+            lab = LabsProfile.objects.get(user=self.request.user.id)
+            
+            data = Visits.objects.filter(lab=lab)
+            return data
+
+        elif self.request.user.role == 'PHARMACIST':
+            pharmacist = PharmacistProfile.objects.get(user=self.request.user.id)
+            
+            data = Visits.objects.filter(pharmacist=pharmacist)
+            return data
+            
+        elif self.request.user.role == 'X_RAYS_LAB':
+            x_rays_lab = X_rays_labProfile.objects.get(user=self.request.user.id)
+           
+            data = Visits.objects.filter(x_rays_lab=x_rays_lab)
+            return data
 
     serializer_class = VisitsSerializer
 
